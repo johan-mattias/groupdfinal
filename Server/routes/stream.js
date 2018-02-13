8e417aa2b36d32ff51859dd5c7f7570c9251aca7
Druvan <edvin.lundberg@gmail.com>
2018-02-13 16:38:20 +0100

    Initial on new endpint

---
+++ Server/routes/stream.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var db = require('../db');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+	
+});
+
+module.exports = router;
