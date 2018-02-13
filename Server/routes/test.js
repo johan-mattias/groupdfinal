729f631e297d1444212716771c111739c9e079bf
Druvan <edvin.lundberg@gmail.com>
2018-02-13 12:34:21 +0100

    added simple rest server

---
+++ Server/routes/test.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+
+    var rawjson = {};
+    var key = 'startbeerlist';
+    rawjson[key] = [];
+    
+
+    var beer = {
+        id: '1',
+        name: 'henieken',
+        type: 'lager'
+    }
+
+
+    var beer2 = {
+        id: '2',
+        name: 'carlsberg',
+        type: 'lager'
+    }
+    rawjson[key].push(beer);
+    rawjson[key].push(beer2);
+    
+    res.send(JSON.stringify(rawjson))
+});
+
+module.exports = router;
