3add07e70d8796540a6f6ee1a0762f6693fa4d40
Druvan <edvin.lundberg@gmail.com>
2018-02-24 19:30:19 +0100

    Fixed fillDummy for image

--- Server/sql/fillDummy.js
+++ Server/sql/fillDummy.js
@@ -6,12 +6,11 @@ var users = "insert into users (name) values ('Edvin');"
 var countries = "insert into countries (name) values ('Netherlands');"
 var beerTypes = "insert into beerTypes (name) values ('Lager');"
 var breweries = "insert into breweries (name) values ('Heineken');"
-var beers = "insert into beers"+
-            "set name = 'Heineken123123123',"+
-            "beertypeID = (select beerTypes.id from beerTypes where id=1),"+
-            "breweryID = (select breweries.id from breweries where id=1),"+
-            "countryID = (select countries.id from countries where id=1)"
-
+var beers = "insert into beers (name, beertypeID, breweryID, countryID) "+
+            "values ('Heineken',1,1,1)"
+var images = "insert into images (link,userID,beerID) "+
+            "values ('https://res.cloudinary.com/teepublic/image/private/s--UvSUNgzW--/b_rgb:fffffe,t_Heather%20Preview/c_lpad,f_jpg,h_630,q_90,w_1200/v1494469473/production/designs/1595608_1.jpg',"+
+            "1,1)"
 
 
 console.log("Connected!");
@@ -37,9 +36,14 @@ db.query(breweries, function (err, result) {
     else console.log("Table breweries filled with dummies");
   });
 
-//   db.query(beers, function (err, result) {
-//     if (err) throw error;
-//     else console.log("Table beers filled with dummies");
-//   });
+  db.query(beers, function (err, result) {
+    if (err) throw error;
+    else console.log("Table beers filled with dummies");
+  });
+
+  db.query(images, function (err, result) {
+    if (err) throw error;
+    else console.log("Table images filled with dummies");
+  });
 
 
