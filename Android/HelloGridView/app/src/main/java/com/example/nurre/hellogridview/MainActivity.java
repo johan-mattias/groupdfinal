40ea99b06102b7a28ccc5296d16956164e263d2c
Nurre <nurre@nurre-g50-70>
2018-02-27 11:57:18 +0100

    Basic gridview application for accesibility. Also dowloads pictures from a hardcoded link.

---
+++ Android/HelloGridView/app/src/main/java/com/example/nurre/hellogridview/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.nurre.hellogridview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                /*Toast.makeText(MainActivity.this, "Bruv: " + position,
+                        Toast.LENGTH_SHORT).show();*/
+                Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
+
