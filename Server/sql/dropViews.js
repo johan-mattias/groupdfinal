10a0c08bb320b711e70091174c6c70cee8072bb6
Druvan <edvin.lundberg@gmail.com>
2018-02-28 16:17:20 +0100

    Initial with stream view

---
+++ Server/sql/dropViews.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+var db = require('../db');
+
+var stream = "drop view stream"
+
+console.log("Connected!");
+db.query(stream, function (err, result) {
+  if (err) console.log("Table commenTs not dropped");
+  else console.log("Table comments dropped");
+  process.kill();
+});
