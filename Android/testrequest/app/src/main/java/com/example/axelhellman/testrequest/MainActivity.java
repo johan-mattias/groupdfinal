e8b4a4fb8929ca0fc62fb49dc874cb1accdd139f
axelhellman <hellmanaxel@gmail.com>
2018-03-02 09:23:23 +0100

    Uploading image to server not working atm

--- Android/testrequest/app/src/main/java/com/example/axelhellman/testrequest/MainActivity.java
+++ Android/testrequest/app/src/main/java/com/example/axelhellman/testrequest/MainActivity.java
@@ -1,269 +1,177 @@
 package com.example.axelhellman.testrequest;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import android.app.Activity;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.SimpleMultiPartRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-
-import java.net.URL;
-import java.net.URLEncoder;
-
-
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ * <p>
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button btnChoose, btnUpload;
 
-    private TextView mResult;
+    public static String URL = "http://188.166.170.111:8080/image/upload"; //Add ip to our server
+    static final int PICK_IMAGE_REQUEST = 1;
+    String filePath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mResult = (TextView) findViewById(R.id.tv_result);
-
-        //make get request
-        //new GetDataTask().execute("http://192.168.10.154:8000/api/status");
-        new GetDataTask().execute("http://130.238.215.190:8000/api/status");
-        //new PostDataTask().execute("http://192.168.10.154:8000/api/status");
-        //new PostDataTask().execute("http://130.238.215.190:8000/api/status");
-        //new PutDataTask().execute();
-    }
-
-    class GetDataTask extends AsyncTask<String, Void, String> {
-
-        ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressDialog = new ProgressDialog(MainActivity.this);
-            progressDialog.setMessage("Loading data..");
-            progressDialog.show();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                return getData(params[0]);
-            } catch (IOException e) {
-                return "Network error Get!";
+        imageView = findViewById(R.id.imageView);
+        btnChoose = findViewById(R.id.button_choose);
+        btnUpload = findViewById(R.id.button_upload);
+        //Display the image selected
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageBrowse();
             }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            //set data response to textView
-            mResult.setText(result);
-            //cancel progress
-            if (progressDialog != null) {
-                progressDialog.dismiss();
-            }
-        }
-
-        private String getData(String urlPath) throws IOException {
-            StringBuilder result = new StringBuilder();
-            BufferedReader bufferedReader = null;
-            //Initialize and config request, then connect to server
-            try {
-                URL url = new URL(urlPath);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setReadTimeout(10000); //miliseconds
-                urlConnection.setConnectTimeout(10000);
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setRequestProperty("Content-Type", "application/json"); //set header
-                urlConnection.connect();
-
-                //Read data response from server
-                InputStream inputStream = urlConnection.getInputStream();
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    result.append(line).append("\n");
-                }
-
-            } finally {
-                if (bufferedReader != null) {
-                    bufferedReader.close();
-                }
-            }
-            return result.toString();
-        }
-    }
-
-    class PostDataTask extends AsyncTask<String, Void, String> {
-
-        ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressDialog = new ProgressDialog(MainActivity.this);
-            progressDialog.setMessage("Inserting dataaaaa..");
-            progressDialog.show();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                return postData(params[0]);
-            } catch (IOException ex) {
-                return "Network error Post!";
-            } catch (JSONException ex) {
-                return "Invalid data!";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-
-            mResult.setText(result);
-            if (progressDialog != null) {
-                progressDialog.dismiss();
-            }
-        }
-
-        private String postData(String urlPath) throws IOException, JSONException {
-
-            StringBuffer result = new StringBuffer();
-            BufferedWriter bufferedWriter = null;
-            BufferedReader bufferedReader = null;
-
-
-            //create data to send to server
-            JSONObject dataToSend = new JSONObject();
-            dataToSend.put("fbname", "Axel = Best in the world");
-            dataToSend.put("content", "Yes he is");
-            dataToSend.put("likes", 1337);
-            dataToSend.put("comments", 1337);
-
-
-
-            try {
-                //intialize and config request then connect to server
-                URL url = new URL(urlPath);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setReadTimeout(10000); //miliseconds
-                urlConnection.setConnectTimeout(10000);
-                urlConnection.setRequestMethod("POST");
-                urlConnection.setRequestProperty("Content-Type", "application/json"); //set header
-                urlConnection.connect();
-
-
-                //write data into server
-                DataOutputStream wr = new DataOutputStream (urlConnection.getOutputStream());
-                wr.writeBytes (String.valueOf(dataToSend));
-                wr.flush ();
-                wr.close ();
-
-
-
-                //Read data response from server
-                InputStream inputStream = urlConnection.getInputStream();
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    result.append(line).append("\n");
-                }
-            } finally {
-                if (bufferedReader != null) {
-                    bufferedReader.close();
-                }
-                if (bufferedWriter != null) {
-                    bufferedWriter.close();
-                }
-            }
-            return result.toString();
-        }
-    }
-
-    class PutDataTask extends AsyncTask<String, Void, String> {
-
-        ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressDialog = new ProgressDialog(MainActivity.this);
-            progressDialog.setMessage("Updating data..");
-            progressDialog.show();
-        }
-
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                return putData(params[0]);
-            } catch (IOException ex) {
-                return "Network error Put!";
-            } catch (JSONException ex) {
-                return "Invalid data!";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-
-            mResult.setText(result);
-            if (progressDialog != null) {
-                progressDialog.dismiss();
-            }
-        }
-
-        private String putData(String urlPath) throws IOException, JSONException {
-
-            BufferedWriter bufferedWriter = null;
-            String result = null;
-            try {
-                //Create data to update
-                JSONObject dataToSend = new JSONObject();
-                dataToSend.put("fbname", "Axel is truly really the best fuck yeah");
-                dataToSend.put("content", "Yes he isikook-update");
-                dataToSend.put("likes", 666);
-                dataToSend.put("comments", 666);
-
-                URL url = new URL(urlPath);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setReadTimeout(10000); //miliseconds
-                urlConnection.setConnectTimeout(10000);
-                urlConnection.setRequestMethod("PUT");
-                urlConnection.setDoOutput(true); //enable output (body data)
-                urlConnection.setRequestProperty("Content-Type", "application/json"); //set header
-                urlConnection.connect();
-
-                OutputStream outputStream = urlConnection.getOutputStream();
-                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-                bufferedWriter.write(String.valueOf(dataToSend.toString()));
-                bufferedWriter.flush();
-
-                if (urlConnection.getResponseCode() == 200) {
-                    return "Update successfully";
+        });
+        //If no image selected show message that a toast with "Image not selected"
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (filePath != null) {
+                    imageUpload(filePath);
                 } else {
-                    return "Update failed";
+                    Toast.makeText(getApplicationContext(), "Image not selected!", Toast.LENGTH_LONG).show();
                 }
-            } finally {
-                if (bufferedWriter != null) {
-                    bufferedWriter.close();
-                }
+
             }
+        });
+    }
+
+
+    private void imageBrowse() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            if(requestCode == PICK_IMAGE_REQUEST){
+                Uri picUri = data.getData();
+
+                filePath = getPath(picUri);
+
+                Log.d("picUri", picUri.toString());
+                Log.d("filePath", filePath);
+                imageView.setImageURI(picUri);
+
+            }
+
         }
+
+    }
+
+    private void imageUpload(final String imagePath) {
+        //A Simple request for making a Multi Part request whose response is retrieve as String
+        SimpleMultiPartRequest smr = new SimpleMultiPartRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
+                        try {
+                            JSONObject jObj = new JSONObject(response);
+                            String message = jObj.getString("message");
+
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            // JSON error
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //smr.addStringParam("user", "1");
+        //smr.addStringParam("beer", "1");
+        //smr.addStringParam("user",  "1");
+        //smr.addStringParam("beer", "1");
+       JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("user", "1");
+            jsonBody.put("beer", "1");
+            jsonBody.put("description", "1");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //smr.addStringParam("user", "1");
+        //smr.addStringParam("beer", "1");
+        //smr.addStringParam("description", "hejasdiuhasfoiuiagsoi");
+        smr.par
+        smr.addFile("image", imagePath);
+        MyApplication.getInstance().addToRequestQueue(smr);
+    }
+
+    private String getPath(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(getApplicationContext(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
     }
 }
