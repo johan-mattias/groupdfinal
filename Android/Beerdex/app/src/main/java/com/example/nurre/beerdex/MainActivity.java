c1a30344bdc2b53703bd8ca24bf71a95894aa957
Nurre <nurre@nurre-g50-70>
2018-03-13 12:44:28 +0100

    Integrated testrequest with Beerdex. Uploading and downloading on Nadroid application is now properly set up. AND pushed up to the git :)

--- Android/Beerdex/app/src/main/java/com/example/nurre/beerdex/MainActivity.java
+++ Android/Beerdex/app/src/main/java/com/example/nurre/beerdex/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.nurre.beerdex;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,15 +12,23 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.util.Log;
+import android.view.View;
 
-import com.android.volley.Response;
-import com.android.volley.request.ImageRequest;
-import com.android.volley.ui.NetworkImageView;
-import com.squareup.picasso.Picasso;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,26 +45,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
-
-        //ImageView imageView = (ImageView) findViewById(R.id.media_image);
-        //Picasso.with(MainActivity.this).load("https://i.ytimg.com/vi/7oVSaUWeKt0/maxresdefault.jpg").into(imageView);
-        //Picasso.with(MainActivity.this).load("").into(imageView);
-
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
         mAdapter = new BeerImageAdapter(MainActivity.this, beerImages);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        final RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
         recyclerView.setAdapter(mAdapter);
 
-        fetchBeerImages();
+        Log.i("MainActivity", "Created recyclerview");
+        myToolbar.findViewById(R.id.upload).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this, "Pressed upload button", Toast.LENGTH_SHORT).show();
+                uploadImage();
+            }
+        });
+        Log.i("MainActivity", "Created onClickListener on backbutton.");
+
+        fetchBeerImages(0);
+        Log.i("MainActivity", "fetchBeerImages(0)");
+        //fetchBeerImages_false();
+        /*recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if(mLayoutManager.findLastCompletely)
+            }
+        });*/
 
     }
 
-    private void fetchBeerImages(){
+    private void fetchBeerImages_false(){
         BeerImage beerImage = new BeerImage("author#1", "Example pictures");
         beerImages.add(beerImage);
 
@@ -76,6 +100,65 @@ public class MainActivity extends AppCompatActivity {
         beerImages.add(beerImage);
     }
 
+    public void uploadImage(){
+        Intent upload = (Intent) new Intent(this, UploadActivity.class);
+        startActivity(upload);
+        //Toast.makeText(MainActivity.this, "You pressed upload, but nothing is happening :P", Toast.LENGTH_LONG);
+    }
 
+    public void fetchBeerImages(final int lastIID){
+
+        @SuppressLint("StaticFieldLeak") AsyncTask<Integer,Void,Void> task = new AsyncTask<Integer, Void, Void>() {
+            @Override
+            protected Void doInBackground(Integer... integers) {
+                OkHttpClient client = new OkHttpClient();
+
+                MediaType mediaType = MediaType.parse( "application/x-www-form-urlencoded" );
+                RequestBody body = RequestBody.create( mediaType, "lastImageID=" + Integer.toString(lastIID) );
+                Request request = new Request.Builder()
+                        .url( "http://188.166.170.111:8080/stream/" )
+                        .post( body )
+                        .addHeader( "content-type", "application/x-www-form-urlencoded" )
+                        .build();
+                try{
+                    Response response = client.newCall(request).execute();
+                    JSONArray jsonArray = new JSONArray(response.body().string());
+                    Log.i("MainActivity", "Fetched JSON-object");
+                    for (int i = 0; i < jsonArray.length(); i++){
+                        JSONObject fetchedBeer = jsonArray.getJSONObject(i);
+
+                        BeerImage newBeerImage = new BeerImage(
+                                fetchedBeer.getInt("image_id"),
+                                fetchedBeer.getString("link"),
+                                fetchedBeer.getString("user_name"),
+                                fetchedBeer.getString("beer_name"),
+                                fetchedBeer.getString("beer_type"),
+                                fetchedBeer.getString("brewery_name"),
+                                fetchedBeer.getString("country"),
+                                fetchedBeer.getString("description")
+                        );
+                        Log.i("MainActivity", "Downloaded picture with ID = " + fetchedBeer.getInt("image_id"));
+                        beerImages.add(newBeerImage);
+                        Log.i("MainActivity", "Added to beerImages.");
+                    }
+
+
+
+                } catch (IOException e){
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    System.out.println("No more content. JSONException when fetching pictures from server.");
+                    e.printStackTrace();
+                }
+                return null;
+            }
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                mAdapter.notifyDataSetChanged();
+            }
+        };
+        task.execute(lastIID);
+    }
 
 }
+
---
+++ Android/Beerdex/app/src/main/java/com/example/nurre/beerdex/UploadActivity.java
@@ -0,0 +1,213 @@
+package com.example.nurre.beerdex;
+
+import android.Manifest;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.SimpleMultiPartRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ * <p>
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class UploadActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button btnChoose, btnUpload;
+    private EditText text_description;
+    private Spinner dropdown_beerselect;
+    public static String URL = "http://188.166.170.111:8080/image/upload"; //Add ip to our server
+    static final int PICK_IMAGE_REQUEST = 1;
+    String filePath;
+    String description;
+    String selected_Beer;
+    //Remove the 1..4 only makes it easier for me to see what number corresponds to a specific beer.
+    String[] beer = new String[]{"1. Lager", "2. Ale", "3. IPA", "4. APA"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+        imageView = findViewById(R.id.imageView);
+        btnChoose = findViewById(R.id.button_choose);
+        btnUpload = findViewById(R.id.button_upload);
+        text_description = findViewById( R.id.textfield_description );
+        dropdown_beerselect = findViewById( R.id.dropdown_beerselect);
+        //Limiting text input to 250 just to make sure we are not sending too long strings..
+        text_description.setFilters(new InputFilter[]{ new InputFilter.LengthFilter(250) });
+
+        //The drop down for beers
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, beer);
+        dropdown_beerselect.setAdapter(adapter);
+
+        //clicking button, then using imageBrowse to display picture
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageBrowse();
+            }
+        });
+        //If no image selected show message it shows a toast that no image has been selected
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                if (filePath != null) {
+                    Toast.makeText(getApplicationContext(), "filepath: "+filePath, Toast.LENGTH_LONG).show();
+                    description = text_description.getText().toString();
+                    selected_Beer = String.valueOf( dropdown_beerselect.getSelectedItemPosition()+1);
+                    imageUpload(filePath);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "No image selected!", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+
+    //Browse the image in app
+    private void imageBrowse() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            if(requestCode == PICK_IMAGE_REQUEST){
+                Uri picUri = data.getData();
+
+                filePath = getPath(picUri);
+
+                Log.d("picUri", picUri.toString());
+                Log.d("filePath", filePath);
+                imageView.setImageURI(picUri);
+
+            }
+
+        }
+
+    }
+
+    private void imageUpload(final String imagePath) {
+        //A Simple request for making a Multi Part request whose response is retrieve as String
+        SimpleMultiPartRequest smr = new SimpleMultiPartRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response from server:", response);
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                        try {
+                            JSONObject jObj = new JSONObject(response);
+                            String message = jObj.getString("first message");
+
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            // JSON error
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        // ACTUALLY GRANT THE APP TO GET PERMISSION TO ACSESS FILE
+        if (ContextCompat.checkSelfPermission(UploadActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //This will print a toast which just tells you if the permission is granted or not
+            //Toast.makeText(UploadActivity.this, "READ permission not granted.", Toast.LENGTH_SHORT).show();
+            //requestReadPermission();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 200);
+        }
+        // ACTUALLY GRANT THE APP TO GET PERMISSION TO ACSESS FILE
+        if (ContextCompat.checkSelfPermission(UploadActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //This will print a toast which just tells you if the permission is granted or not
+            //Toast.makeText(UploadActivity.this, "WRITE permission not granted.", Toast.LENGTH_SHORT).show();
+            //requestWritePermission();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+        }
+
+
+
+            //since no login is implemented we cannot actually tell the id
+            //Adds param userID
+            smr.addStringParam("userID", "1");
+            //Adds param beerID with value from dropdown
+            smr.addStringParam("beerID", "" + selected_Beer);
+            //Do we really need to send this? 
+            smr.addStringParam("imagename", "nurre.jpeg");
+            smr.addStringParam("mimetype", "image/jpeg");
+            smr.addStringParam( "description", "" + description );
+            smr.addFile("image", imagePath);
+            UploadImage.getInstance().addToRequestQueue(smr);
+
+
+        //Toast.makeText(getApplicationContext(), "Enter a description!", Toast.LENGTH_LONG).show();
+
+        //Adding the picture TODO: might have to cast the sending picture to .jpeg
+
+    }
+
+    private String getPath(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(getApplicationContext(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+
+
+
+
+
+}
